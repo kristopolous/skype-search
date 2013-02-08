fa9d9cdb8674174a1425f90d904ca3325cd2002d
Chris McKenzie <kristopolous@yahoo.com>
2013-02-08 05:43:45 -0800

    Displaying the right name now

--- api/search.php
+++ api/search.php
@@ -2,7 +2,7 @@
 $query = mysql_real_escape_string($_GET['q']);
 $db = new SQLite3("main.db");
 
-$qres = $db->query("select id, convo_id, timestamp, author, chatname, body_xml from Messages where body_xml like '%${query}%' order by timestamp desc");
+$qres = $db->query("select id, convo_id, timestamp, from_dispname, chatname, body_xml from Messages where body_xml like '%${query}%' order by timestamp desc");
 
 $res = Array();
 while($row = $qres->fetchArray()) {
