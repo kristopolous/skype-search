6901fcce8d001d7090779e29b93961a5e10ce6e4
Chris McKenzie <kristopolous@yahoo.com>
2013-02-13 00:56:46 -0800

    real names + color coordinated

--- js/skype-search.js
+++ js/skype-search.js
@@ -17,11 +17,51 @@ function doDuration(num) {
   return res.reverse().join(':');
 }
 
-var nameMap = {};
+var 
+  nameMap = {},
+  colorMap = {};
+
+function hsv2rgb(h, s, v) {
+    h = (h % 1 + 1) % 1; // wrap hue
+
+    var i = Math.floor(h * 6),
+        f = h * 6 - i,
+        p = v * (1 - s),
+        q = v * (1 - s * f),
+        t = v * (1 - s * (1 - f));
+
+    switch (i) {
+        case 0: return [v, t, p];
+        case 1: return [q, v, p];
+        case 2: return [p, v, t];
+        case 3: return [p, q, v];
+        case 4: return [t, p, v];
+        case 5: return [v, p, q];
+    }
+}
 $(function(){
+  var h = 0, s = 0.3, v = 0.2;
+  function nextColor() {
+    v += 0.035;
+    h += 0.06;
+    if(v >= 0.5) {
+      v = 0.1;
+    }
+    if(h >= 1) {
+      h = 0;
+    }
+    var mycolor = hsv2rgb(h, s, v);
+    mycolor[0] *= 256;
+    mycolor[1] *= 256;
+    mycolor[2] *= 256;
+    mycolor = _.map(mycolor, Math.floor);
+    return 'rgb(' + mycolor.join(',') + ')';
+  }
+
   $.getJSON("api/whois.php", function(data) {
     _.each(data, function(value, key) {
       nameMap[value.skypename] = value.fullname;
+      colorMap[value.skypename] = nextColor();
     });
   });
 });
@@ -30,7 +70,15 @@ function getName(){
   var value = this.innerHTML;
   if(nameMap[value]) {
     this.innerHTML = nameMap[value];
+    console.log(colorMap[value]);
+    this.style.background = colorMap[value];
   }
+  var cName = value.replace(/[^\w]/g,'');
+
+  $(this).addClass('user-' + cName).hover(
+    function() { $(".user-" + cName).addClass('hover'); },
+    function() { $(".user-" + cName).removeClass('hover'); }
+ );
 }
 
 function showCalls() {
